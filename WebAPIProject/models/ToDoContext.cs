7eda945c62c0186be01faae1aeab0b72bd741821
Manoj Kumar <Manoj Kumar@DESKTOP-037IA34>
2022-06-25 17:09:05 +0530

    Add project files.

---
+++ WebAPIProject/models/ToDoContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace WebAPIProject.models
+{
+    public class ToDoContext:DbContext 
+    {
+        public ToDoContext(DbContextOptions<ToDoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
+    }
+}
